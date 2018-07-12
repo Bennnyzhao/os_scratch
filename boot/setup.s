2b1aaaee89d78e1c1d050a6d478cf7c69a762de7
Benny Zhao <bennnyzhao@gmail.com>
2018-07-13 00:06:43 +0800

    进入保护模式示例，包含代码搬移，设置中断控制器及设置idt

--- boot/setup.s
+++ boot/setup.s
@@ -65,9 +65,28 @@ no_disk1:
     rep
     stosb
     
-is_disk1:    
+is_disk1: 
+    cli
+    
+    mov $0x0000, %ax
+    cld
+do_move:
+    mov %ax, %es
+    add $0x1000, %ax
+    cmp $0x9000, %ax
+    jz end_move
+    mov %ax, %ds
+    xor %si, %si
+    xor %di, %di
+    mov $0x8000, %cx
+    rep
+    movsw
+    jmp do_move
+    
+end_move:     
     mov $INITSEG, %ax
     mov %ax, %ds
+    lidt idt_48
     lgdt gdt_48
     
     call empty_8042
@@ -77,6 +96,33 @@ is_disk1:
     movb $0xDF, %al
     outb %al, $0x60
     call empty_8042
+    
+    movb $0x11, %al
+    outb %al, $0x20
+    .word 0x00EB, 0x00EB
+    outb %al, $0xA0
+    .word 0x00EB, 0x00EB
+    movb $0x20, %al
+    outb %al, $0x21
+    .word 0x00EB, 0x00EB
+    movb $0x28, %al
+    outb %al, $0xA1
+    .word 0x00EB, 0x00EB
+    movb $0x4, %al
+    outb %al, $0x21
+    .word 0x00EB, 0x00EB
+    movb $0x2, %al
+    outb %al, $0xA1
+    .word 0x00EB, 0x00EB
+    movb $0x1, %al
+    outb %al, $0x21
+    .word 0x00EB, 0x00EB
+    outb %al, $0xA1
+    .word 0x00EB, 0x00EB
+    movb $0xFF, %al
+    outb %al, $0x21
+    .word 0x00EB, 0x00EB
+    outb %al, $0xA1
     
     mov $0x0001, %ax
     lmsw %ax
@@ -93,20 +139,24 @@ gdt:
    .word 0, 0, 0, 0
    
    .word 0x07FF
-   .word start_32      # modified
-   .word 0x9A09        # modified
+   .word 0x0000
+   .word 0x9A00
    .word 0x00C0
    
    .word 0x07FF
    .word 0x0000
    .word 0x9200
    .word 0x00C0
+   
+idt_48:
+    .word 0
+    .word 0, 0
     
 gdt_48:
     .word (0x800-1)
     .word gdt, 0x9
-    
-start_32:
+  
+.org 0x800
 .code32
     movl $0x10, %eax
     mov %ax, %ds
@@ -116,7 +166,7 @@ start_32:
     add $0x2, %ebx
     mov $0x0c42, %dx
     movw %dx, (%ebx)
-
-loop:
-    jmp loop
+    .word 0xFEEB
+#loop:
+    #jmp loop
 
