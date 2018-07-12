c68ba4081f82984dc685681e5c33946c490f26a2
Benny Zhao <bennnyzhao@gmail.com>
2018-07-13 00:28:31 +0800

    删除注释掉的多余代码

--- include/asm/system.h
+++ include/asm/system.h
@@ -13,20 +13,6 @@ __asm__("movl %%esp, %%eax\n\t"\
 	"movw %%ax, %%gs"\
 	:::"ax")
 
-/*#define move_to_user_mode() \
-__asm__ ("movl %%esp,%%eax\n\t" \
-    "pushl $0x17\n\t" \
-    "pushl %%eax\n\t" \
-    "pushfl\n\t" \
-    "pushl $0x0f\n\t" \
-    "pushl $1f\n\t" \
-    "iret\n" \
-    "1:\tmovl $0x17,%%eax\n\t" \
-    "movw %%ax,%%ds\n\t" \
-    "movw %%ax,%%es\n\t" \
-    "movw %%ax,%%fs\n\t" \
-    "movw %%ax,%%gs" \
-    :::"ax")*/
 
 #define sti() __asm__ ("sti"::)        // 开中断嵌入汇编宏函数。set interrupt
 #define cli() __asm__ ("cli"::)        // 关中断。clear interrupt
